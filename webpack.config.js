2841077fd7e6c2cef3617176d623b77a3c7241d9
GrigorMkr <grom666@gmail.com>
2024-10-13 22:37:01 +0500

    изменения кода в modified:   webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,57 @@
 const path = require('path');
 const CopyPlugin = require('copy-webpack-plugin');
+const HtmlPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/main.js',
+  devtool: 'source-map',
   output: {
-    filename: 'bundle.js',
     path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.[contenthash].js',
     clean: true,
   },
-  devtool: 'source-map',
   plugins: [
-    new CopyPlugin({
-      patterns: [{ from: 'public' }],
+    new HtmlPlugin({
+      template: 'public/index.html'
     }),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: 'public',
+          globOptions: {
+            ignore: ['**/index.html']
+          }
+        }
+      ]
+    })
   ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              [
+                '@babel/preset-env',
+                {
+                  debug: true,
+                  useBuiltIns: 'usage',
+                  corejs: 3,
+                  loose: true
+                }
+              ]
+
+            ]
+          }
+        }
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ]
+  }
 };
