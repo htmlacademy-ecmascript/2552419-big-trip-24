e5414bd300686ec63875a9d8d344a93b58fa3086
GrigorMkr <grom666@gmail.com>
2024-10-16 01:27:56 +0500

    исправление

--- src/view/sort-view.js
+++ src/view/sort-view.js
@@ -1,22 +1,12 @@
-import { createElement } from '../render';
-import { capitalizeFirstLetter } from '../utils';
-import { SORT_TYPES } from '../const';
+import { createElement } from '../../render.js';
+import { createAllSortTypesTemplate } from './sort-view-components/sort-items-view.js';
 
-function createSortingItemTemplate(type) {
-  return(`
-    <div class="trip-sort__item  trip-sort__item--${type}">
-      <input id="sort-${type}" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-${type}" checked>
-      <label class="trip-sort__btn" for="sort-${type}">${capitalizeFirstLetter(type)}</label>
-    </div>`
-  );
-}
+const createSortItems = createAllSortTypesTemplate();
 
-function createSortTemplate() {
-  return(`
-    <form class="trip-events__trip-sort  trip-sort" action="#" method="get">
-      ${SORT_TYPES.map((type) => createSortingItemTemplate(type)).join('')}
-    </form>`
-  );
+export function createSortTemplate() {
+  return ` <form class="trip-events__trip-sort  trip-sort" action="#" method="get">
+            ${createSortItems}
+          </form>`;
 }
 
 export default class SortView {
